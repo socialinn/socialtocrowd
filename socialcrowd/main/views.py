d4e4d0251386a67bec5dfb7a406a9afd23b3ebf2
Victor Ramirez <virako@wadobo.com>
2014-11-07 17:29:12 +0100

    Created profile view.

--- socialcrowd/main/views.py
+++ socialcrowd/main/views.py
@@ -4,10 +4,16 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 from django.views.generic.base import TemplateView
+from project.models import Shipping
 
 
 class Profile(TemplateView):
     template_name = 'profile.html'
 
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(Profile, self).get_context_data(*args, **kwargs)
+        shippings = Shipping.objects.filter(user=self.request.user.id)
+        ctx['shippings'] = shippings
+        return ctx
 
 profile = login_required(Profile.as_view())
