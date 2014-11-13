a67b69247a3f0b0d5293ee5801d83e15a9b99f3b
Daniel Garcia Moreno <danigm@wadobo.com>
2014-11-13 11:56:53 +0100

    Creating the default Site in the flatpages migration

--- socialcrowd/main/migrations/0001_flatpages.py
+++ socialcrowd/main/migrations/0001_flatpages.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.core.management import call_command
+from django.contrib.sites.models import Site
 
 
 def add_flatpages_data(apps, schema_editor):
+    Site.objects.get_or_create(id=1, name="socialtocrowd.org", domain="socialtocrowd.org")
     call_command('loaddata', 'flatpages.json')
 
 def remove_flatpages_data(apps, schema_editor):
     m = apps.get_model('flatpages', 'FlatPage')
     m.objects.all().delete()
+
+    s = Site.objects.objects.get(id=1, name="socialtocrowd.org", domain="socialtocrowd.org")
+    s.delete()
 
 
 class Migration(migrations.Migration):
