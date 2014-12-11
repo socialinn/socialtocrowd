6b693db4bae9a1dd6f5911267cdc805b974f6176
Daniel Garcia Moreno <danigm@wadobo.com>
2014-12-11 11:02:36 +0100

    Removed top_projects from context_processors, it's not needed

--- socialtocrowd/main/context_processors.py
+++ socialtocrowd/main/context_processors.py
@@ -4,5 +4,4 @@ from project.models import Organization, Project
 def global_vars(request):
     ctx = {}
     ctx['top_ongs'] = Organization.top()
-    ctx['top_projects'] = Project.top()
     return ctx
